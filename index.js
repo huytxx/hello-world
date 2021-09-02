546e7edc3242985358de43e551a4ea774e1c107f
Xuan Huy <xuanhuy900@gmail.com>
2021-09-03 00:35:49 +0700

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send("What'up man ? Welcome")
+  res.send("What'up man ? Welcome hello")
 })
  
 app.listen(3000)
